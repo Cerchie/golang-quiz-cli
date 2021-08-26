07233f404b2344b0c0ffd9ceb46b3e487fa48514
Lucia Cerchie <lucia@stepzen.com>
2021-08-26 12:20:03 -0700

    adds some array questions

--- src/commands/begin.ts
+++ src/commands/begin.ts
@@ -8,11 +8,29 @@ export default class Begin extends Command {
     const answer = await cli.prompt(
       "Welcome to your Golang quiz! Would you prefer a quiz on arrays or maps?"
     );
-    if (answer !== "arrays" || answer !== "maps") {
+    if (answer !== "arrays" && answer !== "maps") {
       this.error(
         chalk.red('Please enter a value of either "maps" or "arrays".')
       );
     }
     this.log(`Great, we'll test your knowledge on ${answer}.`);
+
+    if (answer == "arrays") {
+      const answer1 = await cli.prompt(
+        "Can the length of an array in Golang be changed?"
+      );
+      const answer2 = await cli.prompt(
+        "Let's say that we have two arrays of different lengths. Are they the same type?"
+      );
+      const answer3 = await cli.prompt(
+        "In Golang, everything is passed by value. What does this mean for assigning the value of an array to another variable?"
+      );
+      const data = {
+        data: { 1: answer1, 2: answer2, 3: answer3 },
+      };
+      cli.table(data, answer);
+      //typeerror: this.data.map is not a function
+    }
   }
 }
+// https://www.sohamkamani.com/golang/arrays-vs-slices/ <- questions source
