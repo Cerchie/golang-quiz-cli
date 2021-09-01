d0fecb2237fc2aaa2366f3691a093c62b1bc6f24
Lucia Cerchie <lucia@stepzen.com>
2021-09-01 11:30:51 -0700

    adds inquirer, prettier, and removes all TS issues excepting .table

--- src/commands/begin.ts
+++ src/commands/begin.ts
@@ -1,32 +1,59 @@
-import { Command } from "@oclif/command";
-import cli from "cli-ux";
-const chalk = require("chalk");
+import Command, {flags} from '@oclif/command'
+import cli from 'cli-ux'
+
+import * as inquirer from 'inquirer'
+const chalk = require('chalk')
+
 export default class Begin extends Command {
-  static description = "this command starts the quiz game";
+  static flags = {
+    stage: flags.string({options: ['arrays', 'maps']}),
+  }
+
+  static description = 'this command starts the quiz game'
 
   async run() {
-    const answer = await cli.prompt(
-      "Welcome to your Golang quiz! Would you prefer a quiz on arrays or maps?"
-    );
-    if (answer !== "arrays" && answer !== "maps") {
+    const {flags} = this.parse(Begin)
+    let stage = flags.stage
+    if (!stage) {
+      const responses: any = await inquirer.prompt([
+        {
+          name: 'stage',
+          message:
+            'Welcome to your Golang quiz! Would you prefer a quiz on arrays or maps?',
+          type: 'list',
+          choices: [{name: 'arrays'}, {name: 'maps'}],
+        },
+      ])
+      stage = responses.stage
+    }
+
+    if (stage !== 'arrays' && stage !== 'maps') {
       this.error(
         chalk.red('Please enter a value of either "maps" or "arrays".')
-      );
+      )
     }
-    this.log(`Great, we'll test your knowledge on ${answer}.`);
+    this.log(`Great, we'll test your knowledge on ${stage}.`)
 
-    if (answer == "arrays") {
+    if (stage === 'arrays') {
       const answer1 = await cli.prompt(
-        "Can the length of an array in Golang be changed?"
-      );
+        'Can the length of an array in Golang be changed?'
+      )
       const answer2 = await cli.prompt(
         "Let's say that we have two arrays of different lengths. Are they the same type?"
-      );
+      )
       const answer3 = await cli.prompt(
-        "In Golang, everything is passed by value. What does this mean for assigning the value of an array to another variable?"
-      );
-      const data = [answer1, answer2, answer3];
-      cli.table(data, answer);
+        'In Golang, everything is passed by value. What does this mean for assigning the value of an array to another variable?'
+      )
+      const data = [answer1, answer2, answer3]
+      cli.table(data)
+
+      // output:
+
+      // 0 1 2         3         4         5
+      // ─ ─ ───────── ───────── ───────── ─────────
+      // n o undefined undefined undefined undefined
+      // n o undefined undefined undefined undefined
+      // a s f         a         f         g
     }
   }
 }
