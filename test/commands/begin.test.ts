8b434c687132093e6288b9e792a8b57be58c61fc
Lucia Cerchie <lucia@stepzen.com>
2021-09-09 14:10:26 -0700

    makes testing specific to begin

--- test/commands/begin.test.ts
+++ test/commands/begin.test.ts
@@ -2,16 +2,19 @@ import {expect, test} from '@oclif/test'
 
 describe('begin', () => {
   test
-  .stdout()
-  .command(['begin'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
+    .stdout()
+    .command(['begin', '--stage', 'arrays'])
+    .it('runs arrays --stage arrays', (ctx) => {
+      expect(ctx.stdout).to.contain(
+        'Can the length of an array in Golang be changed?'
+      )
+    })
   test
-  .stdout()
-  .command(['begin', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    .stdout()
+    .command(['begin', '--stage', 'maps'])
+    .it('runs maps --stage maps', (ctx) => {
+      expect(ctx.stdout).to.contain(
+        'Do maps consume more or less memory than arrays'
+      )
+    })
 })
